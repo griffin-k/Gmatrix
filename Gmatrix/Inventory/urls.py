25a120a08fd9083c53117bbeacd0b1f4a44bf072
Karimullah <karimullah.khan3637@gmail.com>
2024-09-06 23:27:27 +0500

    final touches

--- Gmatrix/Inventory/urls.py
+++ Gmatrix/Inventory/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import ProductList, inventory_dashboard, view_inventory, add_product, issue_component
+from .views import ProductList, inventory_dashboard, view_inventory, add_product, issue_component, issued_items_view, return_item
 
 urlpatterns = [
     path('inventory_mgt/', inventory_dashboard, name='inventory_dashboard'),
     path('view_inventory/', view_inventory, name='inventory_view'),
     path('add_products/', add_product, name='product_add'),
     path('api/products/', ProductList.as_view(), name='product_list'),
+    path('issued-items/', issued_items_view, name='issued_items'),
+    path('return-item/<int:item_id>/', return_item, name='return_item'),
 
 
     path('issue-component/', issue_component, name='issue_component'),
--- Gmatrix/Inventory/views.py
+++ Gmatrix/Inventory/views.py
@@ -9,23 +9,37 @@ from .serializers import ProductSerializer
 from django.contrib import messages
 from django.http import HttpResponse
 from .forms import ComponentIssueForm
-
-
-
-
-
-
+from .models import ComponentIssue
+from django.core.paginator import Paginator
 
 
 def inventory_dashboard(request):
     return render(request, 'Inventory/inventory_dashboard.html')
 
 def view_inventory(request):
-    return render(request, 'Inventory/view_inventory.html')
+    product_list = Product.objects.all()  
+    paginator = Paginator(product_list, 10) 
+    page_number = request.GET.get('page')  
+    page_obj = paginator.get_page(page_number)  
+    context = {
+        'page_obj': page_obj, 
+    }
+    return render(request, 'Inventory/view_inventory.html', context)
 
 
-# def issue_item(request):
-#     return render(request, 'Inventory/issue_item.html')
+
+def issued_items_view(request):
+    issued_items_list = ComponentIssue.objects.all()
+    paginator = Paginator(issued_items_list, 10) 
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'Inventory/return_item.html', {'page_obj': page_obj})
+
+def return_item(request, item_id):
+    item = ComponentIssue.objects.get(pk=item_id)
+    item.delete()  
+    return redirect('issued_items')
+
 
 
 
@@ -37,7 +51,6 @@ def issue_component(request):
             return redirect('issue_component')  
     else:
         form = ComponentIssueForm()
-
     return render(request, 'Inventory/issue_item.html', {'form': form})
 
 
@@ -66,15 +79,12 @@ class ProductList(APIView):
         category_filter = request.GET.get('category', 'all')
         status_filter = request.GET.get('status', 'all')
         search_term = request.GET.get('search', '').lower()
-
         products = Product.objects.all()
-
         if category_filter != 'all':
             products = products.filter(category=category_filter)
         if status_filter != 'all':
             products = products.filter(status=status_filter)
         if search_term:
             products = products.filter(model__icontains=search_term)
-
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
