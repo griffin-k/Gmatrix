f05c853dfe84cda67a96460df6616a20524e148a
Karimullah <karimullah.khan3637@gmail.com>
2024-09-10 17:23:32 +0500

    done

--- Gmatrix/Gmatrix/settings.py
+++ Gmatrix/Gmatrix/settings.py
@@ -33,7 +33,11 @@ SECRET_KEY = 'django-insecure-)aki03jxe30any-z5g3p0i3o0k!^((qf7%%2$%&l+ur_1i+337
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['d028-119-155-196-151.ngrok-free.app', 'localhost', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = [
+    'https://d028-119-155-196-151.ngrok-free.app',
+]
+
 
 
 TAILWIND_APP_NAME = 'theme'
