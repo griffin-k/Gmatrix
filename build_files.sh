30e7ab5a3b8459a3a23064d2fa58c60b8ea24bf3
Karimullah <karimullah.khan3637@gmail.com>
2024-09-10 21:24:29 +0500

    added

--- build_files.sh
+++ build_files.sh
@@ -3,6 +3,11 @@
 # Echo a message indicating the start of the build process
 echo "Starting build process with Python 3.9..."
 
+# Ensure pip is installed
+echo "Ensuring pip is installed..."
+python3.9 -m ensurepip --upgrade
+python3.9 -m pip install --upgrade pip
+
 # Step 1: Install Python dependencies
 echo "Installing dependencies using Python 3.9..."
 python3.9 -m pip install -r requirements.txt
@@ -10,8 +15,6 @@ python3.9 -m pip install -r requirements.txt
 # Step 2: Collect static files
 echo "Collecting static files..."
 python3.9 -m manage collectstatic --noinput || { echo 'Collectstatic failed'; exit 1; }
-
-
 
 # Step 3: Apply database migrations
 echo "Applying database migrations..."
